61d63dafae7435d2a619a3c25aa64bb98ad79442
Jahiker Rojas <rojasjahiker@gmail.com>
2023-07-01 01:03:14 -0400

    [add] signup request from de frontent

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,19 +1,58 @@
+import { useRef, useContext } from 'react'
 import { Link } from 'react-router-dom'
 
+import axiosClient from '../axios-client'
+import { StateContext } from '../context/ContextProvider'
+
 const Signup = () => {
+  const nameRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+  const passwordConfirmRef = useRef(null)
+
+  const { setUser, setToken } = useContext(StateContext)
+
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmRef.current.value
+    }
+
+    console.log(payload)
+
+    axiosClient
+      .post('/signup', payload)
+      .then((data) => {
+        setUser(data.user)
+        setToken(data.token)
+      })
+      .catch((err) => {
+        const response = err.response
+        if (response && response.status === 422) {
+          console.log(response.data.error)
+        }
+      })
   }
 
   return (
     <form onSubmit={onSubmit} className='animated fadeInDown'>
       <h1 className='title'>Signup for free</h1>
-      <input type='text' placeholder='Full Name' />
-      <input type='email' placeholder='Email Address' />
-      <input type='password' placeholder='Password' />
-      <input type='password' placeholder='Password Confirmation' />
+      <input ref={nameRef} type='text' placeholder='Full Name' />
+      <input ref={emailRef} type='email' placeholder='Email Address' />
+      <input ref={passwordRef} type='password' placeholder='Password' />
+      <input
+        ref={passwordConfirmRef}
+        type='password'
+        placeholder='Password Confirmation'
+      />
       <button className='btn btn-block'>Register</button>
-      <p className='message'>Already Registered? <Link to='/login'>Sign in</Link> </p>
+      <p className='message'>
+        Already Registered? <Link to='/login'>Sign in</Link>{' '}
+      </p>
     </form>
   )
 }
