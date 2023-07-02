0ce6c385799a2cf2956feddf200d03bb527dcbcb
Jahiker Rojas <rojasjahiker@gmail.com>
2023-07-02 01:57:30 -0400

    [add] auth controller on backend

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * signup method
+     *
+     * @param SignupRequest $request
+     * @return void
+     */
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Login method
+     *
+     * @param LoginRequest $request
+     * @return void
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ]);
+        }
+
+        /** @var App\Models\User $user */
+        $user = Auth::user();
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * logout method
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        /** @var App\Models\User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ], 204);
+    }
+}
